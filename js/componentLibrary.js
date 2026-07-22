da8369cf8a0e4eab0c9543e7e578c50907b54b44
TerabyteEXE <166437365+TerabyteEXE@users.noreply.github.com>
2026-07-22 15:59:36 -0400

    Update componentLibrary.js

--- js/componentLibrary.js
+++ js/componentLibrary.js
@@ -1,1 +1,8 @@
-
+const componentLibrary = [
+  { type: 'Network Switch', color: '#00e5ff', inPorts: 1, outPorts: 1 },
+  { type: 'Audio Mixer', color: '#9d4edd', inPorts: 1, outPorts: 1 },
+  { type: 'Computer / PC', color: '#ff006e', inPorts: 1, outPorts: 1 },
+  { type: 'Display / Monitor', color: '#3a86ff', inPorts: 1, outPorts: 1 },
+  { type: 'Speaker Array', color: '#fb5607', inPorts: 1, outPorts: 1 },
+  { type: 'Microphone RX', color: '#ffbe0b', inPorts: 1, outPorts: 1 }
+];
